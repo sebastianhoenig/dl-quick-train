58fd2a5bc44a1b6ef8aff5cd71712b2f989d5413
Patrick Leask <patrickaaleask@gmail.com>
2025-06-07 11:39:45 +0000

    fix init imports

--- dl_quick_train/__init__.py
+++ dl_quick_train/__init__.py
@@ -1,23 +1,13 @@
 """Utilities for running the dl-quick-train pipeline."""
 
 from .pipeline import (
-    input_fetcher,
-    activation_fetcher,
-    activation_loader,
     new_wandb_process,
     log_stats,
-    train,
     run_pipeline,
-    main,
 )
 
 __all__ = [
-    "input_fetcher",
-    "activation_fetcher",
-    "activation_loader",
     "new_wandb_process",
     "log_stats",
-    "train",
     "run_pipeline",
-    "main",
 ]
